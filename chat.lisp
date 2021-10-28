fc03f02518f90e0484ed55c72c9fc54fab6cbe3e
Your Name <mirielloraffaele@gmail.com>
2021-10-28 12:26:16 +0200

    prime modifiche f3

--- chat.lisp
+++ chat.lisp
@@ -3,18 +3,20 @@
 (defparameter h2 (make-hash-table))
 (defparameter h3 (make-hash-table))
 (defparameter operaPrecedente nil)
-(defparameter defaultAns '(L'opera opera.nome è stata scritta a opera.luogo nel opera.data) )
+(defparameter defaultAns '("L'opera" opera.nome "e' stata scritta a" opera.luogo "nel" opera.data) )
 
 ;riempimento per prova
 ;
-(setf (gethash 'dove h1) '(L'opera opera.nome è stata scritta a opera.luogo nel opera.data))
-(setf (gethash 'quando h1) '(L'opera opera.nome è stata scritta a opera.luogo nel opera.data))
-(setf (gethash 'argomento h1) '(L'opera opera.nome tratta di opera.tema))
-(setf (gethash 'tema h1) '(L'opera opera.nome tratta di opera.tema))
-(setf (gethash '1 h2) '(Opera1 "10-10-2001" "Rende" ))
-(setf (gethash '2 h2) '(Opera2 "12-10-2010" "Maropati" ))
-(setf (gethash '3 h2) '(Opera3 "13-10-2001" "Cosenza" ))
-(setf (gethash '4 h2) '(Opera4 "14-10-2001" "Rende" ))
+;(setf (gethash 'dove h1) '("L'opera" opera.nome "e' stata scritta a" opera.luogo "nel" opera.data))
+;(setf (gethash 'quando h1) '("L'opera" opera.nome "e' stata scritta a" opera.luogo "nel" opera.data))
+(setf (gethash 'argomento h1) '("L'opera" opera.nome "tratta di" opera.tema))
+(setf (gethash 'tema h1) '("L'opera" opera.nome "tratta di" opera.tema))
+(setf (gethash 'chiave h1) '("L'opera" opera.nome "e' scritta in" opera.chiave))
+(setf (gethash 'raccolta h1) '("L'opera" opera.nome "fa parte delle" opera.raccolta))
+(setf (gethash '1 h2) '("Opera1" "2001" "Rende" "Chiave1" "Tema1" "Raccolta1"))
+(setf (gethash '2 h2) '("Opera2" "12-10-2010" "Maropati"))
+(setf (gethash '3 h2) '("Opera3" "13-10-2001" "Cosenza"))
+(setf (gethash '4 h2) '("Opera4" "14-10-2001" "Rende"))
 (setf (gethash 'racc1 h3) '(raccolta n one))
 (setf (gethash 'racc2 h3) '(raccolta n two))
 (setf (gethash 'racc3 h3) '(raccolta n three))
@@ -29,6 +31,12 @@
 ;(terpri)
 
 
+(defun string-to-list (str)
+        (if (not (streamp str))
+           (string-to-list (make-string-input-stream str))
+           (if (listen str)
+               (cons (read str) (string-to-list str))
+               nil)))
 
 (defun f2 (input)
 
@@ -41,7 +49,7 @@
     (dolist (parola input)
 
         (cond ((null b1) 
-        ;se b1 è null, facciamo il lookup in h1  
+        ;se b1 è null, facciamo il lookup in h1
             (setq tmp1 (gethash parola h1))
             (cond ((not (null tmp1)) (setq b1 tmp1))   ) 
             )
@@ -77,21 +85,29 @@
 (defun f3 (input l)
 
     ;a questo punto abbiamo b1, b2, b3
-    (cond ( (not (null b2)) (setq operaPrecedente b2) )  )
+    (cond ( b2 ) (setq operaPrecedente b2)   )
 
     
     (cond 
         ;bool1 && bool2 -> rispondiamo con info su opera indicata
-        ( (and (not (null b1)) (not (null b2)) )  (<COSTRUIRE RISPOSTA CON B1 E B2>) )
+        ( (and b1 b2)
+          (subst (car b2) 'opera.nome (subst (cadddr b2) 'opera.chiave (subst (cadr (cdddr b2)) 'opera.tema (subst (caddr (cdddr b2)) 'opera.raccolta b1))))
+        )
 
         ;!bool2 && bool3 -> rispondiamo con info su raccolta indicata
-        ( (and (null b2) (not (null b3)) ) (<COSTRUIRE RISPOTA CON B3>) ) 
+        ( (and (null b2) b3) 
+          
+        ) 
 
         ;!bool1 && bool2 -> (L'opera opera.nome è stata scritta a opera.luogo nel opera.data) 
-        ( (and (null b1) (not (null b2)) ) (<COSTRUIRE RISPOSTA CON defaultAns e B2>) )
+        ( (and (null b1)  b2 ) 
+          (subst (car b2) 'opera.nome (subst (cadr b2) 'opera.data (subst (caddr b2) 'opera.luogo defaultAns)))
+        )
         
-        ;bool1 && length(input)=1 && !bool2 && operaPrecedente!=null && !bool3 -> gli rispondo con l'opera precedente
-        ( (and (not (null b1)) (= 1 (length input)) (null b2) (not (null operaPrecedente)) (null b3)) (<COSTRUIRE RISPOSTA CON operaPrecedente>) )
+        ;bool1 && length(input)<2 && !bool2 && operaPrecedente!=null && !bool3 -> gli rispondo con l'opera precedente
+        ( (and  b1 (< 2 (length input)) (null b2)  operaPrecedente (null b3)) 
+          (<COSTRUIRE RISPOSTA CON operaPrecedente>)
+        )
     
         ;bool1 && (length(input)>1 ||operaPrecedente==null) && !bool2 && !bool3 -> "Mi dispiace, non conosco quest'opera"
         ( (and (not (null b1)) (or (> 1 (length input)) (null operaPrecedente)) (null b2) (null b3) ) (<COSTRUIRE RISP MI DISPIACE NON CONOSCO QUEST'OPERA>))
@@ -110,8 +126,8 @@
             (input (read-from-string (concatenate 'string "(" line ")")))) 
             (when (string-equal line "q") (return))
             ; qui chiamata f1
-            (write (f3 input (f2 input)))
-            
+            (format t "~{~a~^ ~}" (f3 input (f2 input)))
+            (terpri)
 
             ; qui chiamata f3 che sostituisce la porcheria che sta sotto (commentata)
             ;(setq *bindings* nil)
