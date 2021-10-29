f5274f95bbe3b014ea2ac4fe1bc7e5bf15fb78d5
Giuseppe Seminara <65341868+giSe98@users.noreply.github.com>
2021-10-29 11:43:21 +0200

    Add deleteP function

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ChatBot
+
+**Traccia**: _Sviluppare in LISP un ChatBot che risponda a domande sulle opere di **[Fryderyk Chopin](https://it.wikipedia.org/wiki/Fryderyk_Chopin)**_
+
+**Professore**: _Luigi Palopoli_
+
+**Candidati** del _Gruppo 4_:
+* Giuseppe Tripodi
+* Davide Iacopino
+* Giuseppe Salerno
+* Giuseppe Seminara
+* Francesco Palumbo
+* Raffaele Miriello
+#
---
+++ deleteP.lisp
@@ -0,0 +1,19 @@
+(defun deleteP (string)
+  (setq result "")
+  (
+    dotimes (i (length string))
+      (
+        cond
+          ( (or (string= (char string i) #\,) (string= (char string i) #\.) (string= (char string i) #\!) (string= (char string i) #\?) (string= (char string i) #\:) (string= (char string i) #\;)) 
+            (
+              cond
+                ( (and (/= i (- (length string) 1)) (string/= (char string (+ i 1)) " ")) (setq result (concatenate 'string result " ")) )
+            )
+          )
+          ((= i (- (length string) 1)) (cond ((string/= (char string i) " ") (setq result (concatenate 'string result (string (char string i)))))))
+          (t (setq result (concatenate 'string result (string (char string i)))))
+      )
+  )
+  result
+)
+(write(deleteP "st. rin,ga?c.\" "))
